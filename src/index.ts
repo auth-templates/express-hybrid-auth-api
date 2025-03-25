9d7cf8dc161e15bf486437ef7f7249faeed4750e
dandelionn <michea.paul@yahoo.com>
2025-03-25 21:03:12 +0200

    feat: add dark theme for Swagger API docs

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import swaggerUI from 'swagger-ui-express';
 import swaggerSpec from './swagger';
 import itemRouter from './routes/itemRoutes';
 import cookieParser from 'cookie-parser';
+import path from 'path';
 
 const app = express();
 const port = 3000;
@@ -10,12 +11,17 @@ const port = 3000;
 app.use(cookieParser());
 app.use(express.json());
 
-// Serve Swagger documentation
-app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerSpec));
+console.log("pathanme:", path.join(__dirname, '../public/swagger-dark.css'))
+// Serve the dark CSS file
+app.use('/swagger-dark.css', express.static(path.join(__dirname, '../public/swagger-dark.css')));
 
+// Serve Swagger documentation
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerSpec, {
+    customCssUrl: '/swagger-dark.css' 
+}));
 app.use('/items', itemRouter);
 
 app.listen(port, () => {
-  return console.log(`Express is listening at http://localhost:${port}`);
+  return console.log(`Express is listening at http://localhost:${port}/api-docs`);
 });
 
