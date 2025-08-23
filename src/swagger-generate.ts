7d16e0405e1649799307ef343f1f55da26ee6434
dandelionn <michea.paul@yahoo.com>
2025-08-23 14:39:25 +0300

    ci: add github actions workflow to deploy app image to docker hub

--- src/swagger-generate.ts
+++ src/swagger-generate.ts
@@ -1,9 +1,17 @@
-import { writeFileSync } from 'fs';
+import { writeFileSync, mkdirSync, existsSync } from 'fs';
 import swaggerSpec from './swagger-config.js';
 import * as yaml from 'js-yaml';
+
+// Ensure the directory exists
+const outputDir = './.swagger';
+if (!existsSync(outputDir)) {
+  mkdirSync(outputDir, { recursive: true }); // recursive ensures parent directories are created
+}
 
 // Convert the Swagger JSON to YAML
 const swaggerYaml = yaml.dump(swaggerSpec);
 
-writeFileSync('./.swagger/express-hybrid-auth-api.yml', swaggerYaml);
+// Write YAML to file
+const outputFile = `${outputDir}/express-hybrid-auth-api.yml`;
+writeFileSync(outputFile, swaggerYaml);
 console.log('✅ Swagger JSON generated at .swagger/express-hybrid-auth-api.yml');
